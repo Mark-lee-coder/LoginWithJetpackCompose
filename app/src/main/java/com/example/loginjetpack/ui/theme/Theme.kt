8866b0135bf718a865ee1529a01fe18b6866b4f2
Mark-lee-coder <leonwahungu@gmail.com>
2023-07-04 10:37:03 +0300

    first phase commit

--- app/src/main/java/com/example/loginjetpack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/loginjetpack/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun LoginJetpackTheme(
+fun JetpackComposeDemoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
